627c86086815a937c4b91f3eaf9b7ab93321e703
juanma3108 <alu0100597929@ull.edu.es>
2013-11-13 02:00:16 +0000

    añadido esquema de tdd y matriznyj.rb

--- practica9/matriznyj/lib/matriznyj.rb
+++ practica9/matriznyj/lib/matriznyj.rb
@@ -1,5 +1,83 @@
 require "matriznyj/version"
 
 module Matriznyj
-  # Your code goes here...
+
+  class Matriz
+
+
+	include Comparable
+
+	def initialize(filas, columnas, *args)
+		
+		
+	end
+
+
+
+	def to_s(*args)
+		
+		
+	end
+	
+	
+	def +(other)
+		
+		
+	end
+
+
+	def -(other)
+		
+		
+	end
+	
+	
+	def *(other)
+		
+		
+	end
+
+	def <=>(other)
+		
+		
+	end
+
+end
+
+
+
+
+#============================================================================
+#======================= CLASE MATRIZ DISPERSA ==============================
+#============================================================================
+
+
+
+class Dispersa < Matriz
+
+	def initialize(filas, columnas, *args)
+		
+		
+	end
+
+
+	def esquemaCoordenado()
+		
+		
+	end	
+
+	def +(other)
+		
+		
+	end
+
+	def -(other)
+		
+		
+	end
+
+	def *(other)
+
+	end
+end
 end

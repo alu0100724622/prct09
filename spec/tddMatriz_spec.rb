fa8fc7310e16dbacd1cb232e19c970864cc1d367
juanma3108 <alu0100597929@ull.edu.es>
2013-11-18 22:53:53 +0000

    añadidos mas tests

--- spec/tddMatriz_spec.rb
+++ spec/tddMatriz_spec.rb
@@ -64,6 +64,51 @@ describe "operaciones aritmeticas" do
 		@matriz6 * @matriz5 == Densa.new(3,3,24,24,24,30,30,30,36,36,36)
 		
 	end
+	
+end
 
+describe "operaciones dispersas" do
+	
+	before :each do
+		@matriz1 = Dispersa.new(2,2,0,0,0,1)
+		@matriz2 = Dispersa.new(2,2,0,0,0,2)
+	end
+	
+	it "mostrando la suma" do
+		
+		@matriz1 + @matriz2 == Dispersa.new(2,2,0,0,0,3)
+		
+	end
+	
+	it "mostrando la resta" do
+		
+		@matriz1 - @matriz2 == Dispersa.new(2,2,0,0,0,-1)
+		
+	end
+
+end
+
+
+describe "minimo y maximo" do
+
+	before :each do
+		@matriz1 = Densa.new(2,2,8,4,-2,1)
+		@matriz2 = Dispersa.new(2,2,0,0,0,2)
+	end
+	
+	it "minimo" do
+		
+		@matriz1.minimo.should eq(-2)
+		@matriz2.minimo.should eq(0)
+		
+	end
+	
+	it "maximo" do
+		
+		@matriz1.minimo.should eq(8)
+		@matriz2.minimo.should eq(2)
+		
+	end
+	
 end
 
--- test/tc_matriz.rb
+++ test/tc_matriz.rb
@@ -43,6 +43,18 @@ class Test_Matriz_Densa < Test::Unit::TestCase
 		assert_equal([[4, 2, 4],[10, 5, 10], [16, 8, 16]], Densa.new(3,3,1,2,3,4,5,6,7,8,9).*(Densa.new(3,3,1,0,1,0,1,0,1,0,1)))
 	end
 
+	def minimo
+		
+		assert_equal(1, Densa.new(2,2,3,7,1,8).minimo)
+		
+	end
+
+	def maximo
+		
+		assert_equal(10, Densa.new(2,2,10,4,2,7).maximo)
+		
+	end
+	
 end
 
 
@@ -59,5 +71,17 @@ class Test_Matriz_Dispersa < Test::Unit::TestCase
 		assert_equal([[-1, 0],[-5, 4]], Dispersa.new(2,2,0,0,0,4).-(Dispersa.new(2,2,1,0,5,0)))
 		assert_equal([[1, -2, 3],[-4, 0, 6], [-7, 8, -9]], Dispersa.new(3,3,1,0,3,0,0,6,0,8,0).-(Dispersa.new(3,3,0,2,0,4,0,0,7,0,9)))
 	end
+	
+	def minimo
+		
+		assert_equal(0, Dispersa.new(2,2,0,0,0,4).minimo)
+		
+	end
 
+	def maximo
+		
+		assert_equal(4, Dispersa.new(2,2,0,0,0,4).maximo)
+		
+	end
+	
 end
